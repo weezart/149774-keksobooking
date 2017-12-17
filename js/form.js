f2a7ce2bae51cfd75b90aed36e043e857ca3212b
weezart <weezart@yandex.ru>
2017-12-17 18:02:38 +0300

    Post form data

--- js/form.js
+++ js/form.js
@@ -24,6 +24,18 @@
     inputPriceType();
     inputTimeHandlers();
     inputGuestRooms();
+    formSubmit();
+  };
+
+  var formSubmit = function () {
+    var form = document.querySelector('.notice__form');
+
+    form.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+      window.backend.save(new FormData(form), function () {
+        form.reset();
+      }, window.backend.onError);
+    });
   };
 
   var inputTitleValidation = function () {
